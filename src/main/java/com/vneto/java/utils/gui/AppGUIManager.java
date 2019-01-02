1a619418715ebb922c2f63b7b952a3ec6a2409b5
Viniboneto <vinicius.b.o.neto@gmail.com>
2019-01-02 18:35:40 -0200

    Ajustando tela login gerada pela classe AppGUIManager e sua interacao com usuario

--- src/main/java/com/vneto/java/utils/gui/AppGUIManager.java
+++ src/main/java/com/vneto/java/utils/gui/AppGUIManager.java
@@ -1,15 +1,22 @@
 package com.vneto.java.utils.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.FontMetrics;
 import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.Window;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -38,14 +45,42 @@ public class AppGUIManager {
 		return ret;
 	}
 
+	public static Dimension getScreenSize() {
+		return Toolkit.getDefaultToolkit().getScreenSize();
+	}
+	
 	static String[] showSingleLoginWnd(Component parent, String title,
 			String msg) throws InterruptedException {
 		
-		JFrame wnd = createFormLikeFrame(parent, title, msg, new String[]{"Login", "Password"}, new boolean[]{false, true}, new String[]{"Ok", "Cancel"});
+		JButton btn;
+		Dimension dim = getScreenSize();
+		dim.height /= 3;
+		dim.width /= 3;
+		Rectangle rect = new Rectangle(dim); 
+		rect.x = dim.width; rect.y = dim.height;
+		rect.height = rect.width = 0;
+		String txtBtn;
+		
+		JFrame wnd = createFormLikeFrame(parent, title, msg, new String[]{"Login", "Password"}, new boolean[]{false, true}, 
+				new String[] { "Ok", "Cancel" }, rect/*getScreenSize() */, true);
 		LoginWndAdapter logAdapt = new LoginWndAdapter(); 
 		wnd.addWindowListener(logAdapt);
 		
-		wnd.pack();
+		for (Component comp: ( (JPanel)wnd.getContentPane().getComponent(1) ).getComponents() ) {
+			if( comp instanceof JButton ) {
+				btn = (JButton)comp;
+				btn.addActionListener(logAdapt);
+						
+				if( ( txtBtn = btn.getText().toLowerCase() ).equals("ok") )
+					btn.setActionCommand(LoginWndAdapter.OK_BTN_CMD);
+				else if(txtBtn.equals("cancel"))
+					btn.setActionCommand(LoginWndAdapter.CNCL_BTN_CMD);
+			}
+		}
+		
+//		wnd.setSize(dim);
+//		wnd.pack();
+//		wnd.setLocation(dim.width, dim.height);
 		wnd.setVisible(true);
 		
 		do {
@@ -71,12 +106,20 @@ public class AppGUIManager {
 
 	static JFrame createFormLikeFrame(Component parent, String title, String msg, String[] labels, boolean[] isPasswd,
 			String[] options) {
+		return createFormLikeFrame(parent, title, msg, labels, isPasswd, options, null, false);
+	}
+
+	static JFrame createFormLikeFrame(Component parent, String title, String msg, String[] labels, boolean[] isPasswd,
+			String[] options, Rectangle wndDims, boolean bestFit) {
 		
 		JLabel lbl;
 		JTextField txtFld;
 		JButton btn;
+		FontMetrics fm;
+		Dimension fldDim = null;
 		JFrame frame = new JFrame(title);
 		JPanel panelFlds, panelBtns;
+		String maxLbl = "";
 		int i = 0;
 		
 		if(labels.length != isPasswd.length)
@@ -90,8 +133,19 @@ public class AppGUIManager {
 		
 		if(labels.length > 0) {
 			
+			if(bestFit) {
+				int maxStrSz = 0, strSz;
+				
+				for (String string : labels) {
+					if((strSz = string.length()) > maxStrSz) {
+						maxStrSz = strSz;
+						maxLbl = string;
+					}
+				}
+			}
+			
 			panelFlds = new JPanel();
-			panelFlds.setLayout(new GridLayout(labels.length, 2));
+			panelFlds.setLayout(new GridLayout(labels.length, 2, 10, 10));
 			
 			frame.add(panelFlds);
 					
@@ -104,7 +158,24 @@ public class AppGUIManager {
 				else	
 					txtFld = new JTextField();
 				
-				panelFlds.add(txtFld);
+				panelFlds.add(txtFld, BorderLayout.NORTH);
+
+				if(bestFit) {
+					if(fldDim == null) {
+						fldDim = new Dimension();
+//						fldDim.width = (fm = lbl.getFontMetrics(lbl.getFont())).charsWidth(string.toCharArray(), 0, string.length());
+						fldDim.width = (fm = lbl.getFontMetrics(lbl.getFont())).stringWidth(maxLbl) + 10;
+						fldDim.height = fm.getHeight() << 1;
+					}
+					
+					lbl.setPreferredSize(fldDim);
+					lbl.setSize(fldDim);
+					txtFld.setPreferredSize(fldDim);
+					txtFld.setSize(fldDim);
+					txtFld.setColumns(maxLbl.length());
+					lbl.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+					txtFld.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+				}
 			}
 		}
 			
@@ -118,6 +189,18 @@ public class AppGUIManager {
 				panelBtns.add(btn);
 			}
 		}
+		
+		if(wndDims != null) {
+//			frame.setBounds(wndDims);
+			if(wndDims.x > 0 && wndDims.y > 0)
+				frame.setLocation(wndDims.x, wndDims.y);
+			if(wndDims.height > 0 && wndDims.width > 0)
+				frame.setSize(wndDims.width, wndDims.height);
+			else
+				frame.pack();
+		}
+		else
+			frame.pack();	
 			
 		return frame;
 	}
@@ -155,6 +238,6 @@ public class AppGUIManager {
 			}
 		}
 		
-		return (String[]) fields.toArray();
+		return fields.toArray(new String[0]);
 	}
 }
--- src/main/java/com/vneto/java/utils/gui/LoginWndAdapter.java
+++ src/main/java/com/vneto/java/utils/gui/LoginWndAdapter.java
@@ -1,30 +1,53 @@
 package com.vneto.java.utils.gui;
 
 import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-class LoginWndAdapter extends WindowAdapter {
+class LoginWndAdapter extends WindowAdapter implements ActionListener {
 
+	static final String OK_BTN_CMD = "ok_button"; 
+	static final String CNCL_BTN_CMD = "cancel_button";
+	
 	String login, passwd;
 	
 	@Override
 	public void windowClosing(WindowEvent e) {
+		if(login == null)
+			login = "";
 		
-//		super.windowClosing(e);
-		JFrame frame = (JFrame)e.getSource();
-		String[] ret = AppGUIManager.retrieveWndFields((Window)frame);
-		
-		if(ret == null || ret.length != 2)
-			throw new IllegalArgumentException("Método AppGUIManager.retrieveWndFields() retornou parâmetros "
-					+ "inválidos ao tentar obter login/senha a partir de entradas do usuário, na tela de login");
-		
-		login = ret[0];
-		passwd = ret[1];
-		
+		if(passwd == null)
+			passwd = "";
+
 		super.windowClosing(e);
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch( e.getActionCommand().toLowerCase() ) {
+			case OK_BTN_CMD:
+					JButton btn = (JButton)e.getSource();
+					Window wnd;
+					String[] ret = AppGUIManager.retrieveWndFields(wnd = (Window)btn.getTopLevelAncestor());
+					
+					if(ret == null || ret.length != 2)
+						throw new IllegalArgumentException("Método AppGUIManager.retrieveWndFields() retornou parâmetros "
+								+ "inválidos ao tentar obter login/senha a partir de entradas do usuário, na tela de login");
+					
+					login = ret[0];
+					passwd = ret[1];
+					wnd.dispatchEvent(new WindowEvent(wnd, WindowEvent.WINDOW_CLOSING));
+				break;
+			case CNCL_BTN_CMD:
+					btn = (JButton)e.getSource();
+					wnd = (Window)btn.getTopLevelAncestor();
+					wnd.dispatchEvent(new WindowEvent(wnd, WindowEvent.WINDOW_CLOSING));
+				break;
+		}
+	}
 }
