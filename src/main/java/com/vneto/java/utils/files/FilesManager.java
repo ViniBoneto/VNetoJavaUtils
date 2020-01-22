d878ac8eedac875b50daf18a2e2265dd4ea6cd66
Viniboneto <vinicius.b.o.neto@gmail.com>
2020-01-21 23:41:21 -0300

    Inclusao do metodo saveFile() a classe FilesManager

--- src/main/java/com/vneto/java/utils/files/FilesManager.java
+++ src/main/java/com/vneto/java/utils/files/FilesManager.java
@@ -1,6 +1,9 @@
 package com.vneto.java.utils.files;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.nio.charset.Charset;
@@ -36,4 +39,32 @@ public class FilesManager {
 		return lines;
 	}
 
+	public static void saveFile(File inFile, String outFileNm) throws IOException {
+		
+		if(inFile == null)
+			throw new NullPointerException("parameter imgFile is null");
+		
+		if(!inFile.exists() || !inFile.isFile())
+			throw new FileNotFoundException("File" + inFile.getAbsolutePath() + " not found ou not a regular file");
+		
+		if(!inFile.canRead())
+			throw new SecurityException("File " + inFile.getAbsolutePath() + " can not be opened for reading");
+		
+		File fOut = new File(outFileNm);
+		byte[] buff = new byte[4096];
+		int nBytes;
+		
+		if( !fOut.exists() && !fOut.createNewFile() )
+			throw new FileNotFoundException("Outuput file " + inFile + " was not found and it was not possible to create it");
+		
+		try (
+				FileInputStream fis = new FileInputStream(inFile); 
+				FileOutputStream fos = new FileOutputStream(fOut);
+			) {
+				while ( fis.available() > 0) {
+					nBytes = fis.read(buff);
+					fos.write(buff, 0, nBytes);
+				}	
+		}
+	}	
 }
